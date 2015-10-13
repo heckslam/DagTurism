aae6db04658b720acfb0d09ce09288a0664e9faa
heckslam <heckslam@yandex.ru>
2015-10-14 01:42:19 +0300

    created few fragments and tabs

--- app/src/main/java/ru/devtron/dagturism/MainActivity.java
+++ app/src/main/java/ru/devtron/dagturism/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -11,21 +13,26 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.TabHost;
 
+import ru.devtron.dagturism.adapter.TabsPagerFragmentAdapter;
+
 public class MainActivity extends AppCompatActivity {
 
-    private static final int MAINLAYOUT = R.layout.activity_main;
+    private static final int LAYOUT = R.layout.activity_main;
 
     private Toolbar toolbar;
     private DrawerLayout drawerLayout;
+    private ViewPager viewPager;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
-        setContentView(MAINLAYOUT);
+        setContentView(LAYOUT);
         initToolbar();
         initNavigationView();
+        initTabs();
+
         //initTabs();
     }
 
@@ -40,6 +47,14 @@ public class MainActivity extends AppCompatActivity {
         });
         toolbar.inflateMenu(R.menu.menu);
 
+    }
+
+    private void initTabs() {
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        TabsPagerFragmentAdapter adapter = new TabsPagerFragmentAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
     }
 
     private void initNavigationView() {
---
+++ app/src/main/java/ru/devtron/dagturism/adapter/TabsPagerFragmentAdapter.java
@@ -0,0 +1,45 @@
+package ru.devtron.dagturism.adapter;
+
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import ru.devtron.dagturism.fragment.ExampleFragment;
+
+public class TabsPagerFragmentAdapter extends FragmentPagerAdapter {
+
+    private String[] tabs;
+
+    public TabsPagerFragmentAdapter(FragmentManager fm) {
+        super(fm);
+        tabs = new String[] {
+                "Список",
+                "Карта",
+                "Tab3"
+        };
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return tabs[position];
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return ExampleFragment.getInstance();
+            case 1:
+                return ExampleFragment.getInstance();
+            case 2:
+                return ExampleFragment.getInstance();
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return tabs.length;
+    }
+}
