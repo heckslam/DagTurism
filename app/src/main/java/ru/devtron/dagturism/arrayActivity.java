6e15373eba6d03e51dbf48d3bc771f941fff6099
heckslam <heckslam@yandex.ru>
2015-10-14 00:29:18 +0300

    Start to implementing NavigationView

--- app/src/main/java/ru/devtron/dagturism/arrayActivity.java
+++ app/src/main/java/ru/devtron/dagturism/arrayActivity.java
@@ -3,9 +3,6 @@ package ru.devtron.dagturism;
 import android.app.Activity;
 import android.os.Bundle;
 
-/**
- * Created by Каспсофт on 10.10.2015.
- */
 public class arrayActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
