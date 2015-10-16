e92418b0ca4f88e41eb4fe8e89a64a041dc25b88
Temirhanova <elviratemirhanova@mail.ru>
2015-10-16 09:40:23 +0300

    файлы которых не хватало1

--- app/src/main/java/ru/devtron/dagturism/fragment/arrayFragment.java
+++ app/src/main/java/ru/devtron/dagturism/fragment/arrayFragment.java
@@ -5,12 +5,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import java.io.FileReader;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import ru.devtron.dagturism.R;
